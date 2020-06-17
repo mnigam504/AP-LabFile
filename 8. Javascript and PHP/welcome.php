fc56073dec5bc7ace3d6ab7eddb7b1422858486c
mnigam504 <55240295+mnigam504@users.noreply.github.com>
2020-06-17 21:07:44 +0530

    Add files via upload

---
+++ 8. Javascript and PHP/welcome.php
@@ -0,0 +1,6 @@
+<h1>Welcome</h1>
+Name : <?php echo $_POST["name"]; ?><br>
+ZipCode : <?php echo $_POST["zicode"]; ?><br>
+Email : <?php echo $_POST["email"]; ?><br>
+Mobile : <?php echo $_POST["phoneNumber"]; ?><br>
+
